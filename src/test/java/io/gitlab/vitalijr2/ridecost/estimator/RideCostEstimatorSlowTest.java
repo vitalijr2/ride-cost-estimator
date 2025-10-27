a280ffe8fad4506dc711e898230efd55b9600fca
Vitalij Berdinskih <vitalij_r2@outlook.com>
2025-10-27 16:27:47 +0200

    Improve integration tests

--- src/test/java/io/gitlab/vitalijr2/ridecost/estimator/RideCostEstimatorSlowTest.java
+++ src/test/java/io/gitlab/vitalijr2/ridecost/estimator/RideCostEstimatorSlowTest.java
@@ -3,9 +3,13 @@ package io.gitlab.vitalijr2.ridecost.estimator;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringStartsWith.startsWith;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import io.gitlab.vitalijr2.ridecost.estimator.internal.DistanceByVolumeEstimator;
+import io.gitlab.vitalijr2.ridecost.estimator.internal.VolumeByDistanceEstimator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,12 +23,13 @@ class RideCostEstimatorSlowTest {
   @CsvSource({"0,64.99,475,Mileage", "15.873,-1,475,Price", "15.873,64.99,-99,Distance"})
   void distanceByVolumeEstimator(double mileage, double price, long distance, String expectedName) {
     // when
-    var estimator = assertDoesNotThrow(RideCostEstimator::distanceByVolumeEstimator, "Distance by volume estimator");
+    var estimator = assertDoesNotThrow(RideCostEstimator::distanceByVolumeEstimator, "Instance of RideCostEstimator");
     var exception = assertThrows(IllegalArgumentException.class,
         () -> estimator.estimateCostOfRide(mileage, price, distance));
 
     // then
-    assertThat(exception.getMessage(), startsWith(expectedName));
+    assertAll("Distance by volume estimator", () -> assertInstanceOf(DistanceByVolumeEstimator.class, estimator),
+        () -> assertThat(exception.getMessage(), startsWith(expectedName)));
   }
 
   @DisplayName("Volume by distance estimator: all parameters must be positive")
@@ -32,12 +37,15 @@ class RideCostEstimatorSlowTest {
   @CsvSource({"0,64.99,475,Mileage", "15.873,-1,475,Price", "15.873,64.99,-99,Distance"})
   void volumeByDistanceEstimator(double mileage, double price, long distance, String expectedName) {
     // when
-    var estimator = assertDoesNotThrow(RideCostEstimator::volumeByDistanceEstimator, "Volume by distance estimator");
+    var estimator = assertDoesNotThrow(RideCostEstimator::volumeByDistanceEstimator, "Instance of RideCostEstimator");
     var exception = assertThrows(IllegalArgumentException.class,
         () -> estimator.estimateCostOfRide(mileage, price, distance));
 
     // then
-    assertThat(exception.getMessage(), startsWith(expectedName));
+    assertAll("Volume by distance estimator", () -> assertInstanceOf(VolumeByDistanceEstimator.class, estimator),
+        () -> assertThat(exception.getMessage(), startsWith(expectedName)));
+
+    ;
   }
 
 }
